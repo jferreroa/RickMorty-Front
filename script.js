285c02a3d3f2870ab714d8a2892d2c660a051a1c
Jaime <raferrero10@outlook.es>
2022-02-07 21:45:29 +0100

    img

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const leerPersonajes = async (link) => {
     const a = await fetch("https://rickandmortyapi.com/api/character/1,183")
-    const b = await (a.json())
-    console.log(b)
+    const b = (a.json())
     if(a){
         return b;
     }
 }
 
-const crearCuadroPersonaje = () => {
+const crearCuadroPersonaje = async (elem) => {
+    const linkFoto = elem.image
+    const datosPersonaje = {
+        name: elem.name,
+        status: elem.status,
+        species: elem.species,
+        location: elem.location.name
+    }
+    console.log(datosPersonaje)
+    console.log(linkFoto)
     const container = document.querySelector(".container")
     let personaje = document.createElement("div")
     personaje.className = "personaje"
-    let foto = document.createElement("div")
+    let foto = document.createElement("img")
     foto.className ="foto"
-    foto.innerHTML = "foto"
+    foto.src = linkFoto
+    //foto.setAttribute('url', 'linkFoto')
     let info = document.createElement("div")
     info.className = "info"
     info.innerHTML ="info"
@@ -25,8 +34,9 @@ const crearCuadroPersonaje = () => {
 
 window.onload = async () => {
     const arrpersonajes = await leerPersonajes()
+    console.log(arrpersonajes)
     arrpersonajes.forEach(person => {
         //crear tantos cuadro personajes como sean necesarios
-        crearCuadroPersonaje()
+        crearCuadroPersonaje(person)
     })
 }
